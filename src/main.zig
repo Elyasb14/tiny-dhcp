9da233b2e4d3dd4c14010bcca20ff16b209224af
Elyas Bianchi <elyasb99@gmail.com>
2026-06-02 11:47:20 -0700

    refactor

--- src/main.zig
+++ src/main.zig
@@ -2,41 +2,59 @@ const std = @import("std");
 
 pub const DHCPPacket = struct {
     bootp_header: *BootpHeader,
-    data: []u8,
 
-    pub fn init(bootp_header: *BootpHeader, data: []u8) DHCPPacket {
-        return .{ .bootp_header = bootp_header, .data = data };
+    pub fn init(bootp_header: *BootpHeader) DHCPPacket {
+        return .{ .bootp_header = bootp_header };
     }
 
-    pub fn write_options(self: *DHCPPacket) void {
-        self.data[240] = 53;
-        self.data[241] = 1;
-        self.data[242] = 2;
-        self.data[243] = 54;
-        self.data[244] = 4;
-        self.data[245] = 192;
-        self.data[246] = 168;
-        self.data[247] = 33;
-        self.data[248] = 4;
-        self.data[249] = 1;
-        self.data[250] = 4;
-        self.data[251] = 255;
-        self.data[252] = 255;
-        self.data[253] = 255;
-        self.data[254] = 0;
-        self.data[255] = 51;
-        self.data[256] = 4;
-        self.data[257] = 0;
-        self.data[258] = 0;
-        self.data[259] = 0x0E;
-        self.data[260] = 0x10;
-        self.data[261] = 3;
-        self.data[262] = 4;
-        self.data[263] = 192;
-        self.data[264] = 168;
-        self.data[265] = 33;
-        self.data[266] = 1;
-        self.data[267] = 255;
+    pub fn write_to_buf(self: *DHCPPacket, out: []u8) !void {
+        if (out.len < 236) return error.BufferTooSmall;
+
+        out[0] = self.bootp_header.op;
+        out[1] = self.bootp_header.htype;
+        out[2] = self.bootp_header.hlen;
+        out[3] = self.bootp_header.hops;
+
+        @memcpy(out[4..8], self.bootp_header.xid_be);
+        @memcpy(out[8..10], self.bootp_header.secs_be);
+        @memcpy(out[10..12], self.bootp_header.flags_be);
+        @memcpy(out[12..16], self.bootp_header.ciaddr);
+        @memcpy(out[16..20], self.bootp_header.yiaddr);
+        @memcpy(out[20..24], self.bootp_header.siaddr);
+        @memcpy(out[24..28], self.bootp_header.giaddr);
+        @memcpy(out[28..44], self.bootp_header.chaddr);
+        @memcpy(out[44..108], self.bootp_header.sname);
+        @memcpy(out[108..236], self.bootp_header.file);
+        @memcpy(out[236..240], &DHCP_MAGIC_COOKIE);
+
+        out[240] = 53;
+        out[241] = 1;
+        out[242] = 2;
+        out[243] = 54;
+        out[244] = 4;
+        out[245] = 192;
+        out[246] = 168;
+        out[247] = 33;
+        out[248] = 4;
+        out[249] = 1;
+        out[250] = 4;
+        out[251] = 255;
+        out[252] = 255;
+        out[253] = 255;
+        out[254] = 0;
+        out[255] = 51;
+        out[256] = 4;
+        out[257] = 0;
+        out[258] = 0;
+        out[259] = 0x0E;
+        out[260] = 0x10;
+        out[261] = 3;
+        out[262] = 4;
+        out[263] = 192;
+        out[264] = 168;
+        out[265] = 33;
+        out[266] = 1;
+        out[267] = 255;
     }
 };
 
@@ -77,26 +95,6 @@ pub const BootpHeader = struct {
             .sname = buf[44..108],
             .file = buf[108..236],
         };
-    }
-
-    pub fn write_bootp_header(self: BootpHeader, out: []u8) !void {
-        if (out.len < 236) return error.BufferTooSmall;
-
-        out[0] = self.op;
-        out[1] = self.htype;
-        out[2] = self.hlen;
-        out[3] = self.hops;
-
-        @memcpy(out[4..8], self.xid_be);
-        @memcpy(out[8..10], self.secs_be);
-        @memcpy(out[10..12], self.flags_be);
-        @memcpy(out[12..16], self.ciaddr);
-        @memcpy(out[16..20], self.yiaddr);
-        @memcpy(out[20..24], self.siaddr);
-        @memcpy(out[24..28], self.giaddr);
-        @memcpy(out[28..44], self.chaddr);
-        @memcpy(out[44..108], self.sname);
-        @memcpy(out[108..236], self.file);
     }
 };
 
@@ -151,15 +149,12 @@ pub fn main(init: std.process.Init) !void {
                     bootp_header.siaddr = &[_]u8{ 192, 168, 33, 4 };
                     bootp_header.ciaddr = &[_]u8{ 0, 0, 0, 0 };
 
-                    try bootp_header.write_bootp_header(offer_buf[0..236]);
-                    @memcpy(offer_buf[236..240], &DHCP_MAGIC_COOKIE);
-
-                    var dhcp_packet = DHCPPacket.init(&bootp_header, &offer_buf);
-                    dhcp_packet.write_options();
+                    var dhcp_packet = DHCPPacket.init(&bootp_header);
+                    try dhcp_packet.write_to_buf(&offer_buf);
 
                     try std.Io.net.Socket.send(&server, io, &broadcast_addr, offer_buf[0..268]);
 
-                    std.debug.print("OFFER SENT\n", .{});
+                    std.log.info("OFFER SENT", .{});
                 } else if (data.len >= 1 and data[0] == 3) {
                     // build ACK packet
                     var ack_buf: [300]u8 = undefined;
@@ -170,17 +165,14 @@ pub fn main(init: std.process.Init) !void {
                     bootp_header.siaddr = &[_]u8{ 192, 168, 33, 4 };
                     bootp_header.ciaddr = &[_]u8{ 0, 0, 0, 0 };
 
-                    try bootp_header.write_bootp_header(ack_buf[0..236]);
-                    @memcpy(ack_buf[236..240], &DHCP_MAGIC_COOKIE);
-
-                    var dhcp_packet = DHCPPacket.init(&bootp_header, &ack_buf);
-                    dhcp_packet.write_options();
+                    var dhcp_packet = DHCPPacket.init(&bootp_header);
+                    try dhcp_packet.write_to_buf(&ack_buf);
 
                     try std.Io.net.Socket.send(&server, io, &broadcast_addr, ack_buf[0..268]);
 
-                    std.debug.print("ACK SENT\n", .{});
+                    std.log.info("ACK SENT", .{});
                 } else {
-                    std.debug.print("NOT SUPPORTED\n", .{});
+                    std.log.err("NOT SUPPORTED", .{});
                 }
             }
 
